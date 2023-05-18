9f845f740e57cc8bd5ca40541b6ece05c3cbcd8c
Heiner000 <garrett.heiner@gmail.com>
2023-05-17 18:29:09 -0700

    adds solves

--- 1-character-count.py
+++ 1-character-count.py
@@ -3,7 +3,6 @@
 given a string count each letter in the string and then print the result.
 
 Example 1:
-
 character_count('banana')
 
 Input: 'banana'
@@ -11,22 +10,29 @@ Output (in the console):
 the character b occurs 1 times
 the character a occurs 3 times
 the character n occurs 2 times
-
-Example 2:
-
-character_count('hello world')
-
-Input: 'hello world'
-Output (in the console): 
-the character h occurs 1 times
-the character e occurs 1 times
-the character l occurs 3 times
-the character o occurs 2 times
-the character   occurs 1 times
-the character w occurs 1 times
-the character r occurs 1 times
-the character d occurs 1 times
 '''
 
 def character_count(string):
-  pass
+  count_dict = {}
+  # iterate characters
+  for char in string:
+    # when a character is in the table, increment its value
+    if char in count_dict:
+      count_dict[char] += 1
+    # otherwise add character to table with a value of 1
+    else:
+      count_dict[char] = 1
+  # iterate once to print characters
+  for char in count_dict:
+    print(f'the character {char} occurs {count_dict[char]} times')
+
+# def character_count(string):
+#   # iterates through the string
+#   for i in string:
+#     if string.count(i) == 1:
+#       print(f"the character {i} occurrs {string.count(i)} times")
+#     else:
+#       print(f"the character {i} occurrs {string.count(i)} times")
+      
+character_count("banana")
+character_count("hello world")
--- 3-two-sum.py
+++ 3-two-sum.py
@@ -3,27 +3,49 @@ https://leetcode.com/problems/two-sum/
 3. Two Sum
 
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
 
 Example 1:
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 Example 2:
-
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
 
 Example 3:
-
 Input: nums = [3,3], target = 6
 Output: [0,1]
 '''    
 
+# def two_sum(nums, target):
+#   # I != J, because it says bring 2 nums
+#   for i in range(len(nums)):
+#     # cannot use the same index twice
+#     for j in range(len(nums)):
+#       # nums[i] + nums[j] == target
+#       if i != j and nums[i] + nums[j] == target:
+#         return [i, j]
+
 def two_sum(nums, target):
-  pass
+  table = {}
+  for i, num in enumerate(nums):
+    # target - num = who would match us
+    match_buddy = target - num
+    if match_buddy not in table:
+      table[num] = i
+    else:
+      return [table[match_buddy], i]
+
+
+nums = [2,7,11,15]
+target = 9
+print(two_sum(nums, target))
+nums = [3,2,4]
+target = 6
+print(two_sum(nums, target))
+nums = [3,3]
+target = 6
+print(two_sum(nums, target))
